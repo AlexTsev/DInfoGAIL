88492e753a5efa6093f721dc8a7afd69bfc8f313
AlexTsev <alex.tsevrenis@gmail.com>
2021-09-23 12:54:47 +0300

    update files

---
+++ Diplwmatikh projects/AircraftTrajectories_D_Info_GAIL/myconfig.py
@@ -0,0 +1,23 @@
+myconfig = {
+    'output_dir': '.',
+    'log_dir': './logs',
+    'input_dir': '.',
+    'exp': 0,
+    'path_size': 1000,
+    'mini_batch_size': 50000,
+    'mini_batches': 1500,
+    'gamma': 0.995,
+    'lamda': 0.97,
+    'max_kl': 0.9,
+    'logstd': 0.6,
+    'critic_epochs': 100,
+    'critic_alpha': 0.001,
+    'discriminator_epochs': 100,
+    'discriminator_alpha': 0.0001,
+    'fvp_damping': 0.1,
+    'd-steps': 1,
+    'bcloning_epochs': 100,
+    'bcloning_folds': 10,
+    'env_reward_lambda': 0.01,
+    'plot_dir':'./plots'
+}
